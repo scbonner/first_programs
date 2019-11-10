1a181071a21a49e7f0d083610d2e751604f41043
Ubuntu <ubuntu@ip-172-31-94-214.ec2.internal>
2019-11-10 16:29:06 +0000

    attempting to update yth.rb file again

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -1,18 +1,11 @@
-
-puts 10
-
+puts "Enter a number of years"
 years = gets.chomp # this returns a string
-
 years = years.to_i # this converts a string to an integer
-
 hours = years * 365 * 24
-
-hours = hours.to_s  # this converts the result to a 
-
-                    # string so that we can print it
-
+hours = hours.to_s # this converts the result to a string to print
 puts "That's #{hours} hours."
 
 # write your code here for the other parts.
+
 
 

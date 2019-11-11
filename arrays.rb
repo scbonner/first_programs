b9e0e1d5a74aea800335da8f6a3131e6c0eada31
scbonner <codingbootcamp918@gmail.com>
2019-11-11 01:29:14 +0000

    first submit arrays file

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+puts "Enter a word"  # prompt user for a word
+a = []
+user_input = gets.chomp # collect input
+a << user_input # push into a
+puts "Enter another word"
+user_input = gets.chomp # push into a
+a << user_input
+puts "Enter a third word"
+user_input = gets.chomp
+a << user_input
+puts a.inspect

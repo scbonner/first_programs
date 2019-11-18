1f576b030fe1ae800f3daa0a9b1b47028d6915d7
scbonner <codingbootcamp918@gmail.com>
2019-11-18 21:21:57 +0000

    submit hello_frank

---
+++ hello_frank.rb
@@ -0,0 +1,11 @@
+name = "Frank"
+puts "Hello, #{name}!"
+
+
+
+
+
+
+# Write a short program that prompts 
+#the user for his name, and prints out Hello ! 
+# such as Hello Frank! if the user types in Frank. Use string interpolation.

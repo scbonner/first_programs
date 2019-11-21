26d1fdf2501b6d3059f28214b4914d683d5f59f6
scbonner <codingbootcamp918@gmail.com>
2019-11-21 01:20:52 +0000

    resubmit file

--- leap_year.rb
+++ leap_year.rb
@@ -7,19 +7,19 @@ if beginning_year > ending_year
     puts "Enter a year less than ending year"
 elsif 
     leap_years = Array.new
-    for i in (beginning_year..ending_year)   # check divisible by 400
+    for i in (beginning_year..ending_year)   # check divisible by 200
         if i % 200 == 0
-            leap_years << i  # check if divisible by 100
-          elsif i % 10 == 0    # check if divisble by 4
+            leap_years << i  # check if divisible by 10
+          elsif i % 10 == 0    # check if divisble by 2
           elsif i % 2 == 0
             leap_years << i
           else
         end 
     end
-    puts "leap years between #{beginning_year.to_a} and #{ending_year.to_a} #{leap_years.join(", ").to_a}"
+    puts "leap years between #{beginning_year.to_s} and #{ending_year.to_s} #{leap_years.join(", ").to_s}"
 end
 
-# changed .to_s to .to_a to see what the outcome would look like
+# changed .to_s to .to_i to see what the outcome would look like
 
 
         

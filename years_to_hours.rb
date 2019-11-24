a5cc7ef0898d8918af4cfa1ee5a1e6d70bf8bd62
scbonner <codingbootcamp918@gmail.com>
2019-11-24 18:48:08 +0000

    file updates

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -1,5 +1,8 @@
+puts "Enter first name"   # prompting user to enter first name
+puts "Enter middle initial"  # prompting user to enter middle initial
+puts "Enter last name"  # prompting user to enter last name
 puts "Enter a number of years"
-years = gets.chomp # this returns a string
+years = 25 # this returns a string
 years = years.to_i # this converts a string to an integer
 hours = years * 365 * 24
 hours = hours.to_s # this converts the result to a string to print
@@ -14,20 +17,30 @@ years = 25
 months_per_year = 12 # a total of twelve number does not vary
 hours = 300 * 24
 
-# output variable
-months = 0
+
+puts "Enter a number of years"
+years = gets.chomp # this returns a string
+years = years.to_i # this converts a string to an integer
+hours = years * 365 * 24
+hours = hours.to_s  # this converts the result to a 
+                    # string so that we can print it
+puts "That's #{hours} hours."
+# write your code here for the other parts.
+
+
+
 
 # processing
-print "What is your name? "
-name = gets.chomp
+# print "What is your name? 
+#name = gets.chomp
 
-print "How many years old are you? "
-years = gets.chomp
+#print "How many years old are you? "
+#years = gets.chomp
 
-months = years * months_per_year
+#months = years * months_per_year
 
-puts "#{name}, at #{years} years old, "\
-"you are #{months} months old."
+#puts "#{name}, at #{years} years old, "\
+#"you are #{months} months old."
 
 
 # Write a program which asks the user for a 

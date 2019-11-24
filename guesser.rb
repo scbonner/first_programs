1eaef7773c5b2d49cb61ccd6954590b619d9a6dc
scbonner <codingbootcamp918@gmail.com>
2019-11-24 19:30:01 +0000

    update file

--- guesser.rb
+++ guesser.rb
@@ -2,7 +2,7 @@ secret = rand(100) + 1  # being of guess game through prompts
 loop do  # begin loop block
   puts "Enter a number between 1 to 100"
   guess = gets.chomp.strip.to_i  # changing string to integer
-  if guess is < 1 || guess > 100
+  if guess < 1 || guess > 100
       puts "Your answer is not valid at this time"
   elsif guess == secret
     puts "You guessed the correct number... hooray!"

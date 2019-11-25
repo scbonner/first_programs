706aa039ef3bb81896f1211e75bc272bac169c5e
scbonner <codingbootcamp918@gmail.com>
2019-11-25 01:34:33 +0000

    update prac6.rb file

---
+++ prac6.rb
@@ -0,0 +1,12 @@
+def say_goodbye_name(name)
+    puts "Goodbye " + name + "!"
+end 
+say_goodbye_name("Tom") 
+
+def say_goodbye_name(name1, name2)
+  puts "Goodbye #{name1} and #{name2}!"
+end
+say_goodbye_name("Tom","Teri") # prints out Hello Frank and Sue!
+
+
+  
--- years_to_hours.rb
+++ years_to_hours.rb
@@ -1,8 +1,5 @@
-puts "Enter first name"   # prompting user to enter first name
-puts "Enter middle initial"  # prompting user to enter middle initial
-puts "Enter last name"  # prompting user to enter last name
 puts "Enter a number of years"
-years = 25 # this returns a string
+years = gets.chomp # this returns a string
 years = years.to_i # this converts a string to an integer
 hours = years * 365 * 24
 hours = hours.to_s # this converts the result to a string to print
@@ -15,38 +12,21 @@ puts "Whew"
 name = "Sadric"
 years = 25
 months_per_year = 12 # a total of twelve number does not vary
-hours = 300 * 24
 
-
-puts "Enter a number of years"
-years = gets.chomp # this returns a string
-years = years.to_i # this converts a string to an integer
-hours = years * 365 * 24
-hours = hours.to_s  # this converts the result to a 
-                    # string so that we can print it
-puts "That's #{hours} hours."
-# write your code here for the other parts.
-
-
-
+# output variable
+months = 0
 
 # processing
-# print "What is your name? 
-#name = gets.chomp
+print "What is your name? "
+name = gets.chomp
 
-#print "How many years old are you? "
-#years = gets.chomp
+print "How many years old are you? "
+years = gets
 
-#months = years * months_per_year
+months = years * months_per_year
 
-#puts "#{name}, at #{years} years old, "\
-#"you are #{months} months old."
+puts "#{name}, at #{years} years old, "\
+"you are #{months} months old."
 
 
-# Write a program which asks the user for a 
-# number of years, and then prints out how many 
-# hours are in that many years.  
-# Then it asks for a number of decades, and prints out 
-# the number of minutes are in that many decades. 
-# Then it asks for the user's age, and prints out 
-# the number of seconds old the user is. 
+

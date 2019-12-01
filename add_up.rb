3595fab3e74eda8bd93629bbb7e5e8f0d2a51553
scbonner <codingbootcamp918@gmail.com>
2019-12-01 20:56:53 +0000

    practice for homework questions

--- add_up.rb
+++ add_up.rb
@@ -1,8 +1,7 @@
-def which_hash(name, options={})
-  puts "The hash named #{name} has keys and values as follows:"
-  options.each do |k, v|
-    puts "key #{k} value #{v}"
-  end
-end
-my_hash = {this_one: "Gypsy", age: 24, animal: "Horse", "color" => "Caramel"}
-which_hash("my_hash", my_hash) # prints out the keys and values of the hash
+h_new = {score1: 5, score2: 17}
+puts h_new
+h_new[:score3] = 11   
+puts h_new
+h_new.delete(:score1)
+puts h_new
+
--- hashtoarray.rb
+++ hashtoarray.rb
@@ -1,8 +1,8 @@
-def which_hash(name, options={})
-  puts "The hash named #{name} has keys and values as follows:"
+def which_hash(activity, options={})
+  puts "The hash named #{activity} has keys and values as follows:"
   options.each do |k, v|
     puts "key #{k} value #{v}"
   end
 end
-my_hash = {Activity: "horseback riding", type: "trail", name: "Gypsy", "color" => "caramel"}
+my_hash = {activity: "horseback riding", type: "trail", name: "Gypsy", "color" => "caramel"}
 which_hash("my_hash", my_hash) # prints out the keys and values of the hash

80a4074d1117509c41489d6e6b3ad07b05a89da7
scbonner <codingbootcamp918@gmail.com>
2019-12-06 20:58:27 +0000

    updating variables 1

--- freeCodeCamp.rb
+++ freeCodeCamp.rb
@@ -1,9 +1,11 @@
-first = "Sadric"
-second = "C" 
-last = "Bonner"
+first_name= "Meechie"
+second_initial = "C" 
+last_name = "Young"
+address = "Oaktop"
 
-puts ("What is your " + first)
-puts ("What is your " + second)
-puts ("What is your " + last)
 
-puts ("Is " + first + " your first name")
+puts ("What is your first name " + first_name)
+puts ("What is your second initial " + second_initial)
+puts ("What is your last name " + last_name)
+puts ("What is your address " + address)
+

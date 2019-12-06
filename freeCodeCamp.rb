be203edf85a07a68afc34cc3f30ce69dca74d2fc
scbonner <codingbootcamp918@gmail.com>
2019-12-06 17:42:18 +0000

    practicing freeCodeCamp video

---
+++ freeCodeCamp.rb
@@ -0,0 +1,7 @@
+# create a varible to store info
+character_name = "John"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+puts ("he really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
---
+++ hash_prac.rb
@@ -0,0 +1,13 @@
+names = Hash.new   # The first script creates a hash and adds two key-value pairs into the hash object.
+names[1] = "Judy"  # a hash object is created
+names[2] = "Billy"  # a hash object is created
+
+# {1=>"Judy", 2=>"Billy"}
+
+puts names  # print out Judy and Billy
+
+
+
+
+# The puts method prints the string representation 
+# of the hash to the console. It is also the string literal of the hash.

accef7482afc7c2163493b66e13378391ce6191c
scbonner <codingbootcamp918@gmail.com>
2019-12-09 16:44:21 +0000

    updated and renamed lesson7 to hash_to_array.rb

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -1,23 +1,29 @@
-cars_hash = {
-  "Toyota" => "Avalon"
-  "Honda" => "Acura"
-  "Nissan" => "Maxima"
-  "Volkswagen" => "Jetta"
-  "Cadillac" => "Coupe"
+
+hash = {
+    "toyota" => "camry",
+    "honda" => "accord",
+    "ford" => "escape",
+    "nissan" => "maxima",
+    "chevolet" => "impala"
 }
-puts cars_hash["Toyota"]
+
+p hash.keys
+p hash.values
+
+vocstartsoft:~/environment/first_programs (master) $ ruby hash_to_array.rb
+["toyota", "honda", "ford", "nissan", "chevolet"]
+["camry", "accord", "escape", "maxima", "impala"]
 
 
 
 
-
-#Write a program that collects 5 keys 
-#and 5 values from the user, and stores them
-#in a hash.  Write a function that accepts the
-#hash as optional parameters and prints out an 
-#array of keys and an array of values.  
-#Call the function within your program so you know it 
-#works. (Question: Can you find online information 
-#on Ruby hash methods that will help with this function?)  
-#Call this program hash_to_array.rb in the first_programs 
-#directory, and store it on github as usual.
+=begin
+Write a program that collects 5 keys and 5
+values from the user, and stores them in a hash.  
+Write a function that accepts the hash as optional parameters 
+and prints out an array of keys and an array of values.  Call 
+the function within your program so you know it works. 
+(Question: Can you find online information on Ruby hash 
+methods that will help with this function?)  Call this program 
+hash_to_array.rb in the first_programs directory, and store it on github as usual.
+=end

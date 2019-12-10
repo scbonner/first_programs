d75d87a4b3c16bfe78eae7780c4b9386a265e940
scbonner <codingbootcamp918@gmail.com>
2019-12-10 01:33:26 +0000

    update hash_prac file lines 39 - 56

--- hash_prac.rb
+++ hash_prac.rb
@@ -21,6 +21,8 @@ should look something like this.
 
 
 
+
+=begin
 movies = { Gladitor: 2009, Up: 2014, Harriett: 2019, Transformers: 2009, Us: 2019, Hustlers: 2019, Just_Mercy: 2019 }
 
 p movies[:Gladitor]
@@ -30,6 +32,13 @@ p movies[:Transformers]
 p movies[:Us]
 p movies[:Hustlers]
 p movies[:Just_Mercy]
+=end
+
+
+
+dates = [2009, 2004, 2019, 2009, 2019, 2019, 2019]
+dates.sort
+p dates
 
 
 
@@ -38,6 +47,12 @@ p movies[:Just_Mercy]
 Use the dates from the previous example and store 
 them in an array. Then make your program output 
 the same thing as exercise 3.
+
+Write a program that uses a hash to store a list 
+of movie titles with the year they came out. 
+Then use the puts command to make your program 
+print out the year of each movie to the screen. 
+
 =end
 
 

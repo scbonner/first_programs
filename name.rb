e2c26d91c6f9fcd4ff75ab0cb6081778927012c5
scbonner <codingbootcamp918@gmail.com>
2019-12-10 06:15:37 +0000

    more updates to exercises

--- name.rb
+++ name.rb
@@ -25,7 +25,7 @@ p "Thank you " + "#{name}."
 =end
 
 
-
+=begin
 p "How old are you?"
 age = gets.chomp
 p "In 10 years how old will you be?"
@@ -61,7 +61,7 @@ vocstartsoft:~/environment/first_programs (master) $ ruby name.rb
 80
 "In 40 years how old will you be?"
 120
-
+=end
 
 =begin
 Write a program called age.rb that asks a user 
@@ -69,11 +69,34 @@ how old they are and then tells them how old
 they will be in 10, 20, 30 and 40 years. 
 Below is the output for someone 20 years old.
 =end
+=begin
+name = 5
+
+10.times do |name|
+    name = 10
+end
+
+puts name
+=end
+
+name = 5
+
+10.times do |n|
+    name = "Sadric " * 10.to_i
+end
+
+puts name
 
 
 
 
-
+=begin
+Add another section onto name.rb that prints 
+the name of the user 10 times. You must do this
+without explicitly writing the puts method 10 
+times in a row. Hint: you can use the times 
+method to do something repeatedly.
+=end
 
 
 

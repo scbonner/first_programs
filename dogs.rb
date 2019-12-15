197d0f287b067a143df56b26730ce02a235119a4
scbonner <codingbootcamp918@gmail.com>
2019-12-15 00:10:01 +0000

    updating answer to first question of less9

--- dogs.rb
+++ dogs.rb
@@ -1,19 +1,30 @@
-
+#define class Dog
 class Dog
-    def noise
-        puts "bark bark"
-    end
+  
+  def initialize( name )
+      @name = name.capitalize
+  end
+  
+  def noise
+    puts "Bark Bark Woof Woof"
+  end
 end
 
-object = Dog.new
+object = Dog.new("Bark Bark Woof Woof")
 object.noise
-    
-
-
-
-
 
 =begin
+def initialize(number_of_wheels, type_of_tank, seating_capacity, maximum_velocity)
+    @number_of_wheels = number_of_wheels
+    @type_of_tank = type_of_tank
+    @seating_capacity = seating_capacity
+    @maximum_velocity = maximum_velocity
+  end
+
+  def make_noise
+    "VRRRRUUUUM"
+
+
 Create a program dogs.rb. The program should 
 define a class called Dog with one
 method, noise, that just prints out "bark bark". 

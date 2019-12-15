e467b9f7259f2acf7fb2aaa380ef28dcf2e7928e
scbonner <codingbootcamp918@gmail.com>
2019-12-15 00:34:29 +0000

    updating less9 ques2- extend

--- dogs.rb
+++ dogs.rb
@@ -1,3 +1,5 @@
+
+=begin
 #define class Dog
 class Dog
   
@@ -13,21 +15,45 @@ end
 object = Dog.new("Bark Bark Woof Woof")
 object.noise
 
-=begin
-def initialize(number_of_wheels, type_of_tank, seating_capacity, maximum_velocity)
-    @number_of_wheels = number_of_wheels
-    @type_of_tank = type_of_tank
-    @seating_capacity = seating_capacity
-    @maximum_velocity = maximum_velocity
-  end
-
-  def make_noise
-    "VRRRRUUUUM"
-
-
 Create a program dogs.rb. The program should 
 define a class called Dog with one
 method, noise, that just prints out "bark bark". 
 In the mainline of the program, create an 
 instance of dog and invoke the noise method.
+=end
+
+
+class Dog
+    
+  def initialize( name )
+      @name = name.capitalize
+  end
+  
+  def noise
+      puts "Bark Bark Woof Woof"
+  end
+end
+ 
+class BigDog
+    
+  def initialize( name )
+      @name = name.capitalize
+  end
+    
+  def noise
+        puts "Woof Woof"
+  end
+end
+
+object = BigDog.new("Woof Woof")
+object.noise
+ 
+
+=begin
+Extend dogs.rb from question 1. Define a class called BigDog 
+as a child class of Dog with one method, noise.
+The method should call the noise method in Dog 
+and should then print out "Woof Woof". In the 
+mainline of the program, create an instance of 
+BigDog and invoke the noise method.  
 =end
